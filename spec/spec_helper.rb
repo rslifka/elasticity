7a316782685d4a59aec9cd6c421b79e3ff037aa2
Rob Slifka <robert.slifka@gmail.com>
2012-03-05 22:01:44 -0800

    Require AWS keys only when attempting to re-record VCR cassettes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,3 @@
-if !ENV["AWS_ACCESS_KEY_ID"] || !ENV["AWS_SECRET_KEY"]
-  puts "\n\n\e[32m**********************************************************************************************"
-  puts "Please set \e[0;1mAWS_ACCESS_KEY_ID\e[32m and \e[0;1mAWS_SECRET_KEY\e[32m in your environment to run the tests."
-  puts ""
-  puts "These keys can be found on your AWS Account > Security Credentials page, at the following URL:"
-  puts ""
-  puts "  \e[0;1mhttps://aws-portal.amazon.com/gp/aws/developer/account/index.html?ie=UTF8&action=access-key"
-  puts ""
-  bourne = !ENV['SHELL'].nil? && ENV['SHELL'] =~ /(bash|ksh|zsh)/
-  puts "\e[0mbash, zsh, ksh: #{"\e[35;1mThis is you!\e[0m" if bourne}"
-  puts "  export AWS_ACCESS_KEY_ID=01234"
-  puts "  export AWS_SECRET_KEY=56789"
-  puts ""
-  puts "\e[0mcsh, tcsh: #{"\e[35;1mThis is you!\e[0m" unless bourne}"
-  puts "  setenv AWS_ACCESS_KEY_ID 01234"
-  puts "  setenv AWS_SECRET_KEY 56789"
-  puts ""
-  puts "\e[32m**********************************************************************************************\n\n\n"
-  exit
-end
-
 AWS_ACCESS_KEY_ID = ENV["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_KEY = ENV["AWS_SECRET_KEY"]
 
@@ -43,8 +22,35 @@ VCR.configure do |c|
   c.default_cassette_options = {
     :match_requests_on => [:method, uri_regexp_matcher]
   }
+  c.before_http_request do |_|
+    require_aws_credentials unless VCR.current_cassette.record_mode == :none
+  end
 end
 
 RSpec.configure do |c|
   c.extend VCR::RSpec::Macros
+end
+
+def require_aws_credentials
+  if !ENV["AWS_ACCESS_KEY_ID"] || !ENV["AWS_SECRET_KEY"]
+    puts "\n\e[33m**********************************************************************************************"
+    puts "\e[32mIf you want to record new cassettes, you'll need to provide a set of AWS credentials so"
+    puts "Elasticity can interact with EMR.  These keys can be found on your AWS Account > Security"
+    puts "Credentials page, at the following URL:"
+    puts ""
+    puts "  \e[0;1mhttps://aws-portal.amazon.com/gp/aws/developer/account/index.html?ie=UTF8&action=access-key"
+    puts ""
+    bourne = !ENV['SHELL'].nil? && ENV['SHELL'] =~ /(bash|ksh|zsh)/
+    if bourne
+      puts "\e[32mbash, zsh, ksh:"
+      puts "  export AWS_ACCESS_KEY_ID=01234"
+      puts "  export AWS_SECRET_KEY=56789"
+    else
+      puts "\e[32mcsh, tcsh:"
+      puts "  setenv AWS_ACCESS_KEY_ID 01234"
+      puts "  setenv AWS_SECRET_KEY 56789"
+    end
+    puts "\e[33m**********************************************************************************************\n"
+    exit
+  end
 end
