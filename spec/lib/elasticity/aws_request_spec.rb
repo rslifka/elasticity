cf48df0a16e398ccfcd1418bbcfbc7d1fb4aac0e
Rob Slifka <robert.slifka@gmail.com>
2012-06-02 12:51:22 -0700

    Reformatting.

--- spec/lib/elasticity/aws_request_spec.rb
+++ spec/lib/elasticity/aws_request_spec.rb
@@ -2,17 +2,6 @@ describe Elasticity::AwsRequest do
 
   let(:aws_request) { Elasticity::AwsRequest.new("aws_access_key_id", "aws_secret_access_key") }
 
-  describe ".aws_escape" do
-    it "should escape according to Amazon's rules" do
-      # Don't encode reserved characters
-      Elasticity::AwsRequest.aws_escape("foo-_.~bar").should == "foo-_.~bar"
-      # Encode as %20, not as +
-      Elasticity::AwsRequest.aws_escape("foo bar").should == "foo%20bar"
-      # Percent encode all other characters with %XY, where X and Y are hex characters 0-9 and uppercase A-F.
-      Elasticity::AwsRequest.aws_escape("foo$&+,/:;=?@bar").should == "foo%24%26%2B%2C%2F%3A%3B%3D%3F%40bar"
-    end
-  end
-
   describe "#sign_params" do
     before do
       Time.stub(:now).and_return(Time.at(1302461096))
@@ -78,6 +67,17 @@ describe Elasticity::AwsRequest do
           end
         end
       end
+    end
+  end
+
+  describe ".aws_escape" do
+    it "should escape according to Amazon's rules" do
+      # Don't encode reserved characters
+      Elasticity::AwsRequest.aws_escape("foo-_.~bar").should == "foo-_.~bar"
+      # Encode as %20, not as +
+      Elasticity::AwsRequest.aws_escape("foo bar").should == "foo%20bar"
+      # Percent encode all other characters with %XY, where X and Y are hex characters 0-9 and uppercase A-F.
+      Elasticity::AwsRequest.aws_escape("foo$&+,/:;=?@bar").should == "foo%24%26%2B%2C%2F%3A%3B%3D%3F%40bar"
     end
   end
 
