aeba9b41f404eeb679fafde7ede98ed72ca2641d
Rob Slifka <robert.slifka@gmail.com>
2012-06-02 12:51:01 -0700

    Reformatting.

--- spec/lib/elasticity/aws_request_spec.rb
+++ spec/lib/elasticity/aws_request_spec.rb
@@ -3,7 +3,6 @@ describe Elasticity::AwsRequest do
   let(:aws_request) { Elasticity::AwsRequest.new("aws_access_key_id", "aws_secret_access_key") }
 
   describe ".aws_escape" do
-
     it "should escape according to Amazon's rules" do
       # Don't encode reserved characters
       Elasticity::AwsRequest.aws_escape("foo-_.~bar").should == "foo-_.~bar"
@@ -12,11 +11,9 @@ describe Elasticity::AwsRequest do
       # Percent encode all other characters with %XY, where X and Y are hex characters 0-9 and uppercase A-F.
       Elasticity::AwsRequest.aws_escape("foo$&+,/:;=?@bar").should == "foo%24%26%2B%2C%2F%3A%3B%3D%3F%40bar"
     end
-
   end
 
   describe "#sign_params" do
-
     before do
       Time.stub(:now).and_return(Time.at(1302461096))
     end
@@ -25,11 +22,9 @@ describe Elasticity::AwsRequest do
       signed_params = aws_request.send(:sign_params, {}, "GET", "example.com", "/")
       signed_params.should == "AWSAccessKeyId=aws_access_key_id&SignatureMethod=HmacSHA256&SignatureVersion=2&Timestamp=2011-04-10T18%3A44%3A56.000Z&Signature=jVLfPS056dNmjpCcikBnPmRHJNZ8YGaI7zdmHWUk658%3D"
     end
-
   end
 
   describe "#aws_emr_request" do
-
     before do
       Time.stub(:now).and_return(Time.at(1302461096))
     end
@@ -40,6 +35,7 @@ describe Elasticity::AwsRequest do
 
         context "when :region is specified" do
           let(:request) { Elasticity::AwsRequest.new("aws_access_key_id", "aws_secret_access_key", :region => "eu-west-1")}
+
           it "should request against that region" do
             RestClient.should_receive(:get).with(/elasticmapreduce\.eu\-west\-1\.amazonaws\.com/)
             request.aws_emr_request({})
