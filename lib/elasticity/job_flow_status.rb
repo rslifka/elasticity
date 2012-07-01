2b8d5560126c7e1734323ac3177bb355a2f6bc72
Rob Slifka <robert.slifka@gmail.com>
2012-07-01 16:12:45 -0700

    Prefer single quotes

--- lib/elasticity/job_flow_status.rb
+++ lib/elasticity/job_flow_status.rb
@@ -22,19 +22,19 @@ module Elasticity
     #   /DescribeJobFlowsResponse/DescribeJobFlowsResult/JobFlows/member
     def self.from_member_element(xml_element)
       jobflow = JobFlowStatus.new
-      jobflow.name = xml_element.xpath("./Name").text.strip
-      jobflow.jobflow_id = xml_element.xpath("./JobFlowId").text.strip
-      jobflow.state = xml_element.xpath("./ExecutionStatusDetail/State").text.strip
-      jobflow.last_state_change_reason = xml_element.xpath("./ExecutionStatusDetail/LastStateChangeReason").text.strip
-      jobflow.steps = JobFlowStatusStep.from_members_nodeset(xml_element.xpath("./Steps/member"))
-      jobflow.created_at = Time.parse(xml_element.xpath("./ExecutionStatusDetail/CreationDateTime").text.strip)
-      started_at = xml_element.xpath("./ExecutionStatusDetail/StartDateTime").text.strip
-      jobflow.started_at = (started_at == "") ? (nil) : (Time.parse(started_at))
-      ready_at = xml_element.xpath("./ExecutionStatusDetail/ReadyDateTime").text.strip
-      jobflow.ready_at = (ready_at == "") ? (nil) : (Time.parse(ready_at))
-      jobflow.instance_count = xml_element.xpath("./Instances/InstanceCount").text.strip
-      jobflow.master_instance_type = xml_element.xpath("./Instances/MasterInstanceType").text.strip
-      jobflow.slave_instance_type = xml_element.xpath("./Instances/SlaveInstanceType").text.strip
+      jobflow.name = xml_element.xpath('./Name').text.strip
+      jobflow.jobflow_id = xml_element.xpath('./JobFlowId').text.strip
+      jobflow.state = xml_element.xpath('./ExecutionStatusDetail/State').text.strip
+      jobflow.last_state_change_reason = xml_element.xpath('./ExecutionStatusDetail/LastStateChangeReason').text.strip
+      jobflow.steps = JobFlowStatusStep.from_members_nodeset(xml_element.xpath('./Steps/member'))
+      jobflow.created_at = Time.parse(xml_element.xpath('./ExecutionStatusDetail/CreationDateTime').text.strip)
+      started_at = xml_element.xpath('./ExecutionStatusDetail/StartDateTime').text.strip
+      jobflow.started_at = (started_at == '') ? (nil) : (Time.parse(started_at))
+      ready_at = xml_element.xpath('./ExecutionStatusDetail/ReadyDateTime').text.strip
+      jobflow.ready_at = (ready_at == '') ? (nil) : (Time.parse(ready_at))
+      jobflow.instance_count = xml_element.xpath('./Instances/InstanceCount').text.strip
+      jobflow.master_instance_type = xml_element.xpath('./Instances/MasterInstanceType').text.strip
+      jobflow.slave_instance_type = xml_element.xpath('./Instances/SlaveInstanceType').text.strip
       jobflow
     end
 
