2756db6ea7854c7790d458488c78da2f40360424
Rob Slifka <robert.slifka@gmail.com>
2012-11-28 20:58:01 -0800

    Removed unnecessary cruft

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,7 @@
-require 'rubygems'
-require 'bundler/setup'
-require 'elasticity'
+ENV['RAILS_ENV'] ||= 'test'
 
+require 'elasticity'
 require 'timecop'
 require 'fakefs/spec_helpers'
-
-ENV['RAILS_ENV'] ||= 'test'
 
 Dir[File.join(File.dirname(__FILE__), 'support', '**', '*.rb')].each { |f| require f }
