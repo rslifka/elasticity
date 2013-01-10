7f858d53bc30c40555b1b9c75e6bb410c047416d
David Stumm <david@heyzap.com>
2013-01-09 16:34:00 -0800

    option to make job visible to all users

--- lib/elasticity/job_flow.rb
+++ lib/elasticity/job_flow.rb
@@ -18,6 +18,7 @@ module Elasticity
     attr_accessor :keep_job_flow_alive_when_no_steps
     attr_accessor :ec2_subnet_id
     attr_accessor :placement
+    attr_accessor :visible_to_all_users
 
     attr_reader :access_key
     attr_reader :secret_key
@@ -146,6 +147,7 @@ module Elasticity
       config = jobflow_preamble
       config[:steps] = jobflow_steps
       config[:log_uri] = @log_uri if @log_uri
+      config[:visible_to_all_users] = @visible_to_all_users if @visible_to_all_users
       config[:bootstrap_actions] = @bootstrap_actions.map{|a| a.to_aws_bootstrap_action} unless @bootstrap_actions.empty?
       config
     end
