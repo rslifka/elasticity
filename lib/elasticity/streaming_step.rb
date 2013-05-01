719107130d3958a6f483cf675025b8809aa9baec
Jeremy Tryba <jeremy@placed.com>
2013-05-01 15:42:34 -0700

    Streaming supports partitioner,caches, hadoop args

--- lib/elasticity/streaming_step.rb
+++ lib/elasticity/streaming_step.rb
@@ -11,6 +11,9 @@ module Elasticity
     attr_accessor :mapper
     attr_accessor :reducer
     attr_accessor :arguments
+    attr_accessor :caches
+    attr_accessor :partitioner
+    attr_accessor :arguments
 
     def initialize(input_bucket, output_bucket, mapper, reducer, *arguments)
       @name = 'Elasticity Streaming Step'
@@ -26,7 +29,9 @@ module Elasticity
       step = Elasticity::CustomJarStep.new('/home/hadoop/contrib/streaming/hadoop-streaming.jar')
       step.name = @name
       step.action_on_failure = @action_on_failure
-      step.arguments = ['-input', @input_bucket, '-output', @output_bucket, '-mapper', @mapper, '-reducer', @reducer] + @arguments
+      step.arguments = @arguments + ['-input', @input_bucket, '-output', @output_bucket, '-mapper', @mapper, '-reducer', @reducer]
+      step.arguments += ['-partitioner', @partitioner] if(@partitioner)
+      step.caches = @caches
       step.to_aws_step(job_flow)
     end
 
