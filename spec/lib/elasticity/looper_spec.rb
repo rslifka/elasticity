8e759fba3d3c79fd6fd09ae92861f23ff0f94eb7
Rob Slifka <robert.slifka@gmail.com>
2013-08-16 23:01:36 -0700

    Spec tweaking

--- spec/lib/elasticity/looper_spec.rb
+++ spec/lib/elasticity/looper_spec.rb
@@ -14,6 +14,14 @@ describe Elasticity::Looper do
       end
     end
 
+    context 'and then you should not wait' do
+      it 'does not communication that waiting is about to occur' do
+        client.should_receive(:on_wait).once
+        l = Elasticity::Looper.new(client.method(:on_retry_check), client.method(:on_wait))
+        l.go
+      end
+    end
+
     context 'when no poll interval is specified' do
       it 'should poll every 60 seconds' do
         Elasticity::Looper.any_instance.should_receive(:sleep).with(60)
@@ -30,27 +38,15 @@ describe Elasticity::Looper do
       end
     end
 
-    context 'and then you should not wait' do
-
-      it 'does not communication that waiting is about to occur' do
-        l = Elasticity::Looper.new(client.method(:on_retry_check), client.method(:on_wait))
-        l.go
-        expect(client).to have_received(:on_wait).once
-      end
-
-    end
-
   end
 
   context 'when you should not wait' do
 
     it 'does not communicate that waiting is about to occur' do
-      f = double(:client, on_retry_check: false, on_wait: nil)
-
-      l = Elasticity::Looper.new(f.method(:on_retry_check), f.method(:on_wait))
+      client = double(:client, on_retry_check: false, on_wait: nil)
+      client.should_not receive(:on_wait)
+      l = Elasticity::Looper.new(client.method(:on_retry_check), client.method(:on_wait))
       l.go
-
-      expect(f).to_not have_received(:on_wait)
     end
 
   end
