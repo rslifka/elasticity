634f0ce6d5727e0f43d5e383e5af47df6a6cdfcf
Nick Gerner <nick.gerner@gmail.com>
2013-10-23 13:23:06 -0700

    put desired availability zone inside instance group config
    as per EMR API:
    http://docs.aws.amazon.com/ElasticMapReduce/latest/API/Welcome.html

--- lib/elasticity/job_flow.rb
+++ lib/elasticity/job_flow.rb
@@ -163,6 +163,7 @@ module Elasticity
       preamble[:placement] = {:availability_zone => @placement} if(@placement)
       preamble.merge!(:ec2_subnet_id => @ec2_subnet_id) if @ec2_subnet_id
       preamble[:instances].merge!(:ec2_key_name => @ec2_key_name) if @ec2_key_name
+      preamble[:instances][:placement] = {:availability_zone => @placement} if(@placement)
       preamble
     end
 
