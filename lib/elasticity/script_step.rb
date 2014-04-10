018da320db5571ea8636e569d4cbb99e737134d9
Robert Slifka <robert.slifka@gmail.com>
2014-04-10 05:48:45 -0700

    Fix for Issue #30

--- lib/elasticity/script_step.rb
+++ lib/elasticity/script_step.rb
@@ -6,6 +6,7 @@ module Elasticity
       @name = 'Elasticity Script Step'
       @jar = 's3://elasticmapreduce/libs/script-runner/script-runner.jar'
       @arguments = ["#{script_name} #{script_args.join(' ')}"]
+      @action_on_failure = 'TERMINATE_JOB_FLOW'
     end
 
   end
