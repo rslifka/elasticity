049812c6bd76c9c38d09740aa4a237262b911c79
Robert Slifka <robert.slifka@gmail.com>
2014-04-30 12:07:18 -0700

    Fixed argument specification

--- lib/elasticity/s3distcp_step.rb
+++ lib/elasticity/s3distcp_step.rb
@@ -2,14 +2,9 @@ module Elasticity
 
   class S3DistCpStep < CustomJarStep
 
-    def initialize(options)
+    def initialize
+      super('/home/hadoop/lib/emr-s3distcp-1.0.jar')
       @name = 'Elasticity S3DistCp Step'
-      @jar = '/home/hadoop/lib/emr-s3distcp-1.0.jar'
-      @action_on_failure = 'TERMINATE_JOB_FLOW'
-      @arguments = []
-      options.each do |argument, value|
-        @arguments << '--arg' << argument.to_s << '--arg' << value
-      end
     end
 
   end
--- spec/lib/elasticity/s3distcp_step_spec.rb
+++ spec/lib/elasticity/s3distcp_step_spec.rb
@@ -2,11 +2,9 @@ describe Elasticity::S3DistCpStep do
 
   it { should be_a Elasticity::CustomJarStep }
 
-  subject { Elasticity::S3DistCpStep.new({'arg1' => 'value1', :arg2 => 'value2'}) }
-
   its(:name) { should == 'Elasticity S3DistCp Step' }
   its(:jar) { should == '/home/hadoop/lib/emr-s3distcp-1.0.jar' }
-  its(:arguments) { should == %w(--arg arg1 --arg value1 --arg arg2 --arg value2) }
+  its(:arguments) { should == [] }
   its(:action_on_failure) { should == 'TERMINATE_JOB_FLOW' }
 
 end
