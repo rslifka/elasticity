176b7fb08d9a561dc3328add9ba9303f71200f7b
Robert Slifka <robert.slifka@gmail.com>
2015-04-24 19:55:03 -0700

    Extract AWS preparation

---
+++ lib/elasticity/aws_utils.rb
@@ -0,0 +1,48 @@
+module Elasticity
+
+  class AwsUtils
+
+    # Escape a string according to Amazon's rules.
+    # See: http://docs.amazonwebservices.com/AmazonSimpleDB/2007-11-07/DeveloperGuide/index.html?REST_RESTAuth.html
+    def self.aws_escape(param)
+      param.to_s.gsub(/([^a-zA-Z0-9._~-]+)/n) do
+        '%' + $1.unpack('H2' * $1.size).join('%').upcase
+      end
+    end
+
+    # Since we use the same structure as AWS, we can generate AWS param names
+    # from the Ruby versions of those names (and the param nesting).
+    def self.convert_ruby_to_aws(params)
+      result = {}
+      params.each do |key, value|
+        case value
+          when Array
+            prefix = "#{camelize(key.to_s)}.member"
+            value.each_with_index do |item, index|
+              if item.is_a?(String)
+                result["#{prefix}.#{index+1}"] = item
+              else
+                convert_ruby_to_aws(item).each do |nested_key, nested_value|
+                  result["#{prefix}.#{index+1}.#{nested_key}"] = nested_value
+                end
+              end
+            end
+          when Hash
+            prefix = "#{camelize(key.to_s)}"
+            convert_ruby_to_aws(value).each do |nested_key, nested_value|
+              result["#{prefix}.#{nested_key}"] = nested_value
+            end
+          else
+            result[camelize(key.to_s)] = value
+        end
+      end
+      result
+    end
+
+    def self.camelize(word)
+      word.to_s.gsub(/\/(.?)/) { '::' + $1.upcase }.gsub(/(^|_)(.)/) { $2.upcase }
+    end
+
+  end
+
+end
