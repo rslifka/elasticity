675d7c8238f6fa3b666d2d231b553f6490acf0f2
Benjamin Fradet <benjamin.fradet@gmail.com>
2017-02-27 13:38:16 +0000

    Make script steps work in any region

--- lib/elasticity/script_step.rb
+++ lib/elasticity/script_step.rb
@@ -2,9 +2,9 @@ module Elasticity
 
   class ScriptStep < CustomJarStep
 
-    def initialize(script_name, *script_args)
+    def initialize(region, script_name, *script_args)
       @name = 'Elasticity Script Step'
-      @jar = 's3://elasticmapreduce/libs/script-runner/script-runner.jar'
+      @jar = "s3://#{region}.elasticmapreduce/libs/script-runner/script-runner.jar"
       @arguments = [script_name].concat(script_args)
       @action_on_failure = 'TERMINATE_JOB_FLOW'
     end
--- spec/lib/elasticity/script_step_spec.rb
+++ spec/lib/elasticity/script_step_spec.rb
@@ -1,7 +1,7 @@
 describe Elasticity::ScriptStep do
 
   subject do
-    Elasticity::ScriptStep.new('script_location', 'arg1', 'arg2')
+    Elasticity::ScriptStep.new('us-east-1', 'script_location', 'arg1', 'arg2')
   end
 
   it { should be_a Elasticity::CustomJarStep }
@@ -9,7 +9,8 @@ describe Elasticity::ScriptStep do
   describe '.initialize' do
     it 'should set the fields appropriately' do
       expect(subject.name).to eql('Elasticity Script Step')
-      expect(subject.jar).to eql('s3://elasticmapreduce/libs/script-runner/script-runner.jar')
+      expect(subject.jar)
+        .to eql('s3://us-east-1.elasticmapreduce/libs/script-runner/script-runner.jar')
       expect(subject.arguments).to eql(%w(script_location arg1 arg2))
     end
   end
